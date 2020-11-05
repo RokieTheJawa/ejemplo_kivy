f6caf3ff45c2d27788f4c8cba85ca3bc806f925e
RokieTheJawa <72667052+RokieTheJawa@users.noreply.github.com>
2020-11-04 23:44:42 -0600

    se agregaron los demás ejemplos

--- radiobuttons.py
+++ radiobuttons.py
@@ -1,20 +1,21 @@
 import kivy
 
 from kivy.config import Config
-#Config.write()
-#Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'height', '300')
+#Config.set('graphics', 'resizable', '0') 
 Config.set('graphics', 'width', '300')
+Config.set('graphics', 'height', '350')
 Config.write()
 
 from kivy.app import App
 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.togglebutton import ToggleButton
 
 def on_text1(instance, value):
     print('The widget', instance, 'have:', value)
@@ -26,9 +27,11 @@ def on_text2(instance, value):
 class BoxLayoutDemo(App):
 
     def build(self):
-        self.title = 'Suma de dos numeros'
+        self.title = 'Calculadora'
         
         superBox= BoxLayout(orientation='vertical', size_hint_x= None,padding=( 10, 0, 0, 10))
+        caja1 = GridLayout(cols=2, row_force_default=True, row_default_height=40)
+        caja2 = GridLayout(cols=2, row_force_default=True, row_default_height=40)
         l1 = Label(text='Valor uno',halign='left', size_hint=(None,None),size= (75, 40))
         l1.bind(size=l1.setter('texture_size'))    
         
@@ -38,10 +41,17 @@ class BoxLayoutDemo(App):
         l2 = Label(text='Valor dos',halign='left', size_hint=(None,None),size= (75, 40))
         l2.bind(size=l2.setter('texture_size')) 
         
+        self.etiqueta1=Label(text='Sumar',size_hint=(None,None),size= (75, 40))
+        self.check1 = CheckBox(size_hint=(None,None),size= (75, 40),group="operacion")
+        
+        self.etiqueta2=Label(text='Restar',size_hint=(None,None),size= (75, 40))
+        self.check2 = CheckBox(size_hint=(None,None),size= (75, 40),group="operacion")
+
         self.textinput2 = TextInput(size_hint=(None,None),size= (175, 50))
         self.textinput2.bind(text=on_text2)
 
-        button1= Button(text="Sumar",size_hint=(None,None),size= (75, 50))
+
+        button1= Button(text="Ejecutar",size_hint=(None,None),size= (75, 50))
         button1.bind(on_press=self.buttonClicked)
         
 
@@ -51,7 +61,16 @@ class BoxLayoutDemo(App):
         superBox.add_widget(l1)
         superBox.add_widget(self.textinput1)
         superBox.add_widget(l2)
+
         superBox.add_widget(self.textinput2)
+        caja1.add_widget(self.etiqueta1)
+        caja1.add_widget(self.check1)
+        caja2.add_widget(self.etiqueta2)
+        caja2.add_widget(self.check2)
+        
+        superBox.add_widget(caja1)
+        superBox.add_widget(caja2)
+
         superBox.add_widget(button1)
         
         superBox.add_widget(self.l3)
@@ -63,10 +82,20 @@ class BoxLayoutDemo(App):
         try:
          valor1=float(self.textinput1.text)
          valor2=float(self.textinput2.text)
-         valor3=valor1+valor2
-         self.l3.text="{}+{}={}".format(valor1,valor2,valor3)
+         if self.check1.active:
+          valor3=valor1+valor2
+          self.l3.text="{}+{}={}".format(valor1,valor2,valor3)
+         if self.check2.active:
+          valor3=valor1-valor2
+          self.l3.text="{}-{}={}".format(valor1,valor2,valor3)
+          
         except:
-         self.l3.text="Se introdujeron datos erroneos!!"   
+         self.l3.text="Se introducioeron datos erroneos!!";   
+
+    
+   
+
+ 
 
 # Instantiate and run the kivy app
 
